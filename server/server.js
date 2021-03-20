9279339a7ef9860ae1a6a642e20a1e621ba9604e
Monica Bui <monicabbui@gmail.com>
2021-03-20 09:34:30 -0700

    Implemented answers section for get questions

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Create helper promise function
 const getQuestions = (product_id) => {
-  const questionSqlQuery = `SELECT * FROM questions WHERE product_id = ${product_id};` // Need work
+  const questionSqlQuery = `SELECT * FROM questions WHERE product_id = ${product_id};`
   return new Promise ((resolve, reject) => {
     db.connection.query(questionSqlQuery, (error, result) => {
       if (error) {
@@ -22,9 +22,20 @@ const getQuestions = (product_id) => {
   })
 }
 
-const getAnswers = () => {
-
+const getAnswers = (question_id) => {
+  const answerSqlQuery = `SELECT * FROM answers WHERE question_id = ${question_id};`
+  return new Promise ((resolve, reject) => {
+    db.connection.query(answerSqlQuery, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    })
+  })
 }
+
+
 
 const getPhotos = () => {
 
@@ -33,9 +44,65 @@ const getPhotos = () => {
 
 // Get request for questions
 app.get('/qa/questions', ((request, response) => {
-  const { product_id } = req.query;
+  // const product_id = 14034;
+  const { product_id } = request.query;
+  let questionsResponse = {
+    product_id: product_id,
+    results: [],
+  };
 
   getQuestions(product_id)
+    .then((result) => {
+      result.forEach(({ id, body, date_written, asker_name, helpful, reported }) => {
+        let questionTableData = {
+          question_id: id,
+          question_body: body,
+          question_date: date_written,
+          asker_name: asker_name,
+          question_helpfulness: helpful,
+          reported: !!reported,
+          answers: {}
+        };
+        let answerTableData = {};
+        getAnswers(questionTableData.question_id)
+        .then((answerResult) => {
+          // response.send(answerResult);
+          // console.log('questionTableData: ', questionTableData);
+          // console.log('THIS IS ANSWER RESULT: ', answerResult);
+            // if (answerResult.length !== 0) {
+            answerResult.forEach((answer, index) => {
+              // { id, question_id, body, date_written, answerer_name, answerer_email, reported, helpfulness }
+                answerTableData = {
+                id: answer.id,
+                body: answer.body,
+                date: answer.date_written,
+                answerer_name: answer.answerer_name,
+                helpfulness: answer.helpfulness,
+                photos: []
+              };
+              questionTableData.answers[ `${answer.id}` ] = answerTableData;
+            })
+            // }
+            // console.log('this is answer table data: ', answerTableData);
+
+            console.log('Question Table Data: ', questionTableData)
+
+          })
+          .catch((error) => {
+            console.log('Error getting answers: ', error);
+          })
+        questionsResponse.results.push(questionTableData);
+      })
+      console.log('Question Response: ', questionsResponse);
+      response.send(questionsResponse);
+    })
+    .catch((error) => {
+      console.log('Error with getQuestions: ', error);
+    })
+
+
+
+
     // .then((result) => {
 
     // })
@@ -59,16 +126,7 @@ app.get('/qa/questions', ((request, response) => {
 // Get request for answers
 app.get('/qa/questions/:question_id/answers', ((request, response) => {
   // const { question_id } = ;
-  const answerSqlQuery = 'SELECT * FROM answers WHERE question_id = ;' // Need work as well
-  db.query(answerSqlQuery, (error, result) => {
-    if (error) {
-      console.log('Error retrieving answers from database: ', error);
-      response.sendStatus(500);
-    } else {
-      console.log('Successfully retrieved answers from database');
-      response.send(result);
-    }
-  })
+
 }))
 
 // Post request for adding a question
@@ -160,28 +218,3 @@ app.put('/qa/answers/:answer_id/report', ((request, response) => {
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
-
-
-let questionsResponse = {};
-getQuestions(14034)
-  .then((result) => {
-    console.log(result)
-    result.forEach(({ id, body, date_written, asker_name, helpful, reported }) => {
-
-      let questionTableData = {
-        question_id: id,
-        question_body: body,
-        question_date: date_written,
-        asker_name: asker_name,
-        question_helpfulness: helpful,
-        reported: !!reported
-      };
-
-      console.log('THIS IS QUESTION TABLE DATA', questionTableData);
-
-
-    })
-  })
-  .catch((error) => {
-    console.log('getQuestions: ', error);
-  })
